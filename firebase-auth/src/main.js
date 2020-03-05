54f088a967794c519edcf2166b968fc102d37d67
Hugh <hugh@crablab.co.uk>
2020-03-05 20:07:10 +0000

    Add Firebase, Vue Router and VueX libraries Import in main application and define configuration structure

--- firebase-auth/src/main.js
+++ firebase-auth/src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import * as firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const configOptions = {
+  apiKey: "",
+  authDomain: "vue-firebase-auth-def4c.firebaseapp.com",
+  databaseURL: "https://vue-firebase-auth-def4c.firebaseio.com",
+  projectId: "vue-firebase-auth-def4c",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+firebase.initializeApp(configOptions);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
