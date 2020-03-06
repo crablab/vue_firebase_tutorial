0d89b6d9970cc35bb86c7401104b1cde74c87ec6
Hugh <hugh@crablab.co.uk>
2020-03-06 01:39:28 +0000

    Add bootstrap-vue for prettiness :sparkles:

--- firebase-auth/src/main.js
+++ firebase-auth/src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./routes/index";
 import * as firebase from "firebase";
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import router from "./routes/index";
 import store from "./store";
 
 Vue.config.productionTip = false;
@@ -21,6 +26,11 @@ firebase.initializeApp(configOptions);
 firebase.auth().onAuthStateChanged(user => {
   store.dispatch("fetchUser", user);
 });
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
